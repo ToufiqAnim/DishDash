12cac5cf4663b22e5e6a4ff92b7e5e6f5d874221
ToufiqAnim <toufiq9610@gmail.com>
2024-04-04 04:34:36 +0600

    [Frontend] user login and logout functionality added

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,16 +1,23 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from './ui/button';
+import UserMenu from './UserMenu';
 
 const MainNav = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
-    <Button
-      variant="ghost"
-      className="font-bold  border-2 border-orange-500 text-lg text-orange-500  "
-      onClick={async () => await loginWithRedirect()}
-    >
-      Log In
-    </Button>
+    <span className="flex sace-x-2 items-center">
+      {isAuthenticated ? (
+        <UserMenu />
+      ) : (
+        <Button
+          variant="ghost"
+          className="font-bold  border-2 border-orange-500 text-lg text-orange-500  "
+          onClick={async () => await loginWithRedirect()}
+        >
+          Log In
+        </Button>
+      )}
+    </span>
   );
 };
 
