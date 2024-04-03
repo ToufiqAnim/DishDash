adbc2fc749f24288f95cd26a2175023a17c0df62
ToufiqAnim <toufiq9610@gmail.com>
2024-04-04 05:12:04 +0600

    [Frontend] MobileNav login and logout functionality added

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,4 +1,4 @@
-import { Menu } from 'lucide-react';
+import { CircleUserRound, Menu } from 'lucide-react';
 import {
   Sheet,
   SheetContent,
@@ -8,8 +8,11 @@ import {
 } from './ui/sheet';
 import { Separator } from './ui/separator';
 import { Button } from './ui/button';
+import { useAuth0 } from '@auth0/auth0-react';
+import MobileNavLinks from './MobileNavLinks';
 
 const MobileNav = () => {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
   return (
     <div>
       <Sheet>
@@ -18,11 +21,27 @@ const MobileNav = () => {
         </SheetTrigger>
         <SheetContent className="space-y-3">
           <SheetTitle>
-            <span>Welcome To DishDash!</span>
+            {isAuthenticated ? (
+              <span className="flex items-center font-bold gap-2">
+                <CircleUserRound />
+                {user?.name}
+              </span>
+            ) : (
+              <span>Welcome To DishDash!</span>
+            )}
           </SheetTitle>
           <Separator />
-          <SheetDescription className="flex">
-            <Button className="flex-1 font-bold bg-green-500">Log In</Button>
+          <SheetDescription className="flex flex-col gap-4">
+            {isAuthenticated ? (
+              <MobileNavLinks />
+            ) : (
+              <Button
+                className="flex-1 font-bold bg-green-500"
+                onClick={async () => await loginWithRedirect()}
+              >
+                Log In
+              </Button>
+            )}
           </SheetDescription>
         </SheetContent>
       </Sheet>
