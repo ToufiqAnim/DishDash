739f4117379607a88f0ca56c7e9a2724c281e6b2
ToufiqAnim <toufiq9610@gmail.com>
2024-04-06 06:54:10 +0600

    [Frontend] react-query added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,22 @@ import ReactDOM from 'react-dom/client';
 import './global.css';
 import { RouterProvider } from 'react-router-dom';
 import routes from './routes/routes';
-
 import AuthProvider from './auth/AuthProvider';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={routes} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={routes} />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
