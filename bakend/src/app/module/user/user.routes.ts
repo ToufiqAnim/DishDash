488119d0d4799085ce0746d2bf0159aa98f109c4
ToufiqAnim <toufiq9610@gmail.com>
2024-04-10 05:19:29 +0600

    Access Token validation done

--- bakend/src/app/module/user/user.routes.ts
+++ bakend/src/app/module/user/user.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { UserController } from './user.controller';
+import { jwtCheck } from '../../middleware/auth';
 
 const router = express.Router();
 
 //api/v1/user
-router.post('/', UserController.createCurrentUser);
+router.post('/', jwtCheck, UserController.createCurrentUser);
 
 export const UserRoutes = router;
