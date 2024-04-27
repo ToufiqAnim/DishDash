d1be881bd887ecea98554c800a7e6d48a3b00d13
ToufiqAnim <toufiq9610@gmail.com>
2024-04-27 15:56:28 +0600

    [Backend]validation middleware created

--- bakend/src/app/module/user/user.routes.ts
+++ bakend/src/app/module/user/user.routes.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import { UserController } from './user.controller';
 import { jwtCheck, jwtParse } from '../../middleware/auth';
+import { validateUserRequest } from '../../middleware/validation';
 
 const router = express.Router();
 
 //api/v1/user
 router.post('/', jwtCheck, UserController.createCurrentUser);
-router.put('/', jwtCheck, jwtParse, UserController.updateCurrentUser);
+router.put(
+  '/',
+  jwtCheck,
+  jwtParse,
+  validateUserRequest,
+  UserController.updateCurrentUser
+);
 
 export const UserRoutes = router;
